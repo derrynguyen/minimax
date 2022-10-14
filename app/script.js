75522ee2de0b94226e42859b042cd783d2144ea6
Derry <thanhhaivevo@gmail.com>
2022-10-14 10:59:40 +0700

    first commit

---
+++ app/script.js
@@ -0,0 +1,234 @@
+
+var board = [
+	[0, 0, 0],
+	[0, 0, 0],
+	[0, 0, 0],
+];
+
+var HUMAN = -1;
+var COMP = +1;
+
+/* Chức năng đánh giá trạng thái heuristic. */
+function evalute(state) {
+	var score = 0;
+
+	if (gameOver(state, COMP)) {
+		score = +1;
+	}
+	else if (gameOver(state, HUMAN)) {
+		score = -1;
+	} else {
+		score = 0;
+	}
+
+	return score;
+}
+
+/* Chức năng này kiểm tra nếu một người chơi cụ thể chiến thắng */
+function gameOver(state, player) {
+	var win_state = [
+		[state[0][0], state[0][1], state[0][2]],
+		[state[1][0], state[1][1], state[1][2]],
+		[state[2][0], state[2][1], state[2][2]],
+		[state[0][0], state[1][0], state[2][0]],
+		[state[0][1], state[1][1], state[2][1]],
+		[state[0][2], state[1][2], state[2][2]],
+		[state[0][0], state[1][1], state[2][2]],
+		[state[2][0], state[1][1], state[0][2]],
+	];
+
+	for (var i = 0; i < 8; i++) {
+		var line = win_state[i];
+		var filled = 0;
+		for (var j = 0; j < 3; j++) {
+			if (line[j] == player)
+				filled++;
+		}
+		if (filled == 3)
+			return true;
+	}
+	return false;
+}
+
+/* Chức năng này kiểm tra nếu con người hoặc máy tính chiến thắng */
+function gameOverAll(state) {
+	return gameOver(state, HUMAN) || gameOver(state, COMP);
+}
+
+function emptyCells(state) {
+	var cells = [];
+	for (var x = 0; x < 3; x++) {
+		for (var y = 0; y < 3; y++) {
+			if (state[x][y] == 0)
+				cells.push([x, y]);
+		}
+	}
+
+	return cells;
+}
+
+/* Một nước đi hợp lệ nếu ô đã chọn trống */
+function validMove(x, y) {
+	var empties = emptyCells(board);
+	try {
+		if (board[x][y] == 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	} catch (e) {
+		return false;
+	}
+}
+
+/* Đặt nước đi trên bảng, nếu tọa độ hợp lệ */
+function setMove(x, y, player) {
+	if (validMove(x, y)) {
+		board[x][y] = player;
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+/* *** Chức năng AI lựa chọn nước đi tốt nhất *** */
+function minimax(state, depth, player) {
+	var best;
+
+	if (player == COMP) {
+		best = [-1, -1, -1000];
+	}
+	else {
+		best = [-1, -1, +1000];
+	}
+
+	if (depth == 0 || gameOverAll(state)) {
+		var score = evalute(state);
+		return [-1, -1, score];
+	}
+
+	emptyCells(state).forEach(function (cell) {
+		var x = cell[0];
+		var y = cell[1];
+		state[x][y] = player;
+		var score = minimax(state, depth - 1, -player);
+		state[x][y] = 0;
+		score[0] = x;
+		score[1] = y;
+
+		if (player == COMP) {
+			if (score[2] > best[2])
+				best = score;
+		}
+		else {
+			if (score[2] < best[2])
+				best = score;
+		}
+	});
+
+	return best;
+}
+
+/* Nó gọi hàm minimax */
+function aiTurn() {
+	var x, y;
+	var move;
+	var cell;
+
+	if (emptyCells(board).length == 9) {
+		x = parseInt(Math.random() * 3);
+		y = parseInt(Math.random() * 3);
+	}
+	else {
+		move = minimax(board, emptyCells(board).length, COMP);
+		x = move[0];
+		y = move[1];
+	}
+
+	if (setMove(x, y, COMP)) {
+		cell = document.getElementById(String(x) + String(y));
+		cell.innerHTML = "O";
+	}
+}
+
+/* main */
+function clickedCell(cell) {
+	var button = document.getElementById("bnt-restart");
+	button.disabled = true;
+	var conditionToContinue = gameOverAll(board) == false && emptyCells(board).length > 0;
+
+	if (conditionToContinue == true) {
+		var x = cell.id.split("")[0];
+		var y = cell.id.split("")[1];
+		var move = setMove(x, y, HUMAN);
+		if (move == true) {
+			cell.innerHTML = "X";
+			if (conditionToContinue)
+				aiTurn();
+		}
+	}
+	if (gameOver(board, COMP)) {
+		var lines;
+		var cell;
+		var msg;
+
+		if (board[0][0] == 1 && board[0][1] == 1 && board[0][2] == 1)
+			lines = [[0, 0], [0, 1], [0, 2]];
+		else if (board[1][0] == 1 && board[1][1] == 1 && board[1][2] == 1)
+			lines = [[1, 0], [1, 1], [1, 2]];
+		else if (board[2][0] == 1 && board[2][1] == 1 && board[2][2] == 1)
+			lines = [[2, 0], [2, 1], [2, 2]];
+		else if (board[0][0] == 1 && board[1][0] == 1 && board[2][0] == 1)
+			lines = [[0, 0], [1, 0], [2, 0]];
+		else if (board[0][1] == 1 && board[1][1] == 1 && board[2][1] == 1)
+			lines = [[0, 1], [1, 1], [2, 1]];
+		else if (board[0][2] == 1 && board[1][2] == 1 && board[2][2] == 1)
+			lines = [[0, 2], [1, 2], [2, 2]];
+		else if (board[0][0] == 1 && board[1][1] == 1 && board[2][2] == 1)
+			lines = [[0, 0], [1, 1], [2, 2]];
+		else if (board[2][0] == 1 && board[1][1] == 1 && board[0][2] == 1)
+			lines = [[2, 0], [1, 1], [0, 2]];
+
+		for (var i = 0; i < lines.length; i++) {
+			cell = document.getElementById(String(lines[i][0]) + String(lines[i][1]));
+			cell.style.color = "red";
+		}
+
+		msg = document.getElementById("message");
+		msg.innerHTML = "Bạn thua!";
+	}
+	if (emptyCells(board).length == 0 && !gameOverAll(board)) {
+		var msg = document.getElementById("message");
+		msg.innerHTML = "Hòa!";
+	}
+	if (gameOverAll(board) == true || emptyCells(board).length == 0) {
+		button.value = "Chơi lại";
+		button.disabled = false;
+	}
+}
+
+/* Restart the game*/
+function restartBnt(button) {
+	if (button.value == "Bắt đầu") {
+		aiTurn();
+		button.disabled = true;
+	}
+	else if (button.value == "Chơi lại") {
+		var htmlBoard;
+		var msg;
+
+		for (var x = 0; x < 3; x++) {
+			for (var y = 0; y < 3; y++) {
+				board[x][y] = 0;
+				htmlBoard = document.getElementById(String(x) + String(y));
+				htmlBoard.style.color = "#444";
+				htmlBoard.innerHTML = "";
+			}
+		}
+		button.value = "Bắt đầu";
+		msg = document.getElementById("message");
+		msg.innerHTML = "";
+	}
+}
